0b243c9c16cc2e1ce7c0cb9b0bfe61a39fb2df73
Xbar <wishyx@gmail.com>
2014-09-17 21:24:46 -0700

    add comment

--- run_phylo.py
+++ run_phylo.py
@@ -7,6 +7,7 @@ import pandas as pd
 datafile = 'NCBI2.csv'
 notefile = 'annotation.txt'
 go_file = 'humanGO.txt'
+# Build tree
 phyloTree = phylo.phylo_score(datafile, 'Homo_sapiens', notefile)
 go_list = pd.read_csv(go_file, sep='\t', names=['source', 'bioentity_label', 'bioentity_name', 'evidence_type'])
 go_gene_set = set(go_list['bioentity_label'])
